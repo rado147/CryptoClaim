ba0a17d80f82483424e379ac5b6aafaafcad7a88
Radoslav Filipov <radoslav.filipov@sap.com>
2019-01-05 17:53:43 +0200

    Add mock beans in tests

--- src/test/java/cf/cryptoclaim/CryptoClaimApplicationTests.java
+++ src/test/java/cf/cryptoclaim/CryptoClaimApplicationTests.java
@@ -3,12 +3,30 @@ package cf.cryptoclaim;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import cf.cryptoclaim.auth.JWTService;
+import cf.cryptoclaim.crypto.ClaimEncryptionService;
+import cf.cryptoclaim.repositories.ConsumedJWTRepository;
+import cf.cryptoclaim.repositories.CryptoMessagesRepository;
+import cf.cryptoclaim.repositories.UsersRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CryptoClaimApplicationTests {
 
+	@MockBean
+	private UsersRepository usersRepository;
+	@MockBean
+	private CryptoMessagesRepository ryptoMessagesRepository;
+	@MockBean
+	private ConsumedJWTRepository consumedJWTRepository;
+	@MockBean
+	private JWTService jwtService;
+	@MockBean
+	private ClaimEncryptionService claimEncryptionService;
+	
 	@Test
 	public void contextLoads() {
 	}
